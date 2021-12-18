713dc5e2ccd82a2497799b4d3e522f6abacb3f0d
Mirko <mirkokawa7@gmail.com>
2021-12-18 14:34:24 +0300

    styblithasion

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-  <div class="container-sm section">
+<div class="container-sm section">
     <div class="row">
       <div class="col">
         <h2><span>MyProject</span></h2>
@@ -35,12 +35,13 @@
   foreach ($row as $val) {
     ?>
       <div class="col">
-        <div class="card h-100 card border-warning mb-3 text-center" style="width: 18rem;" >
-          <img src=<?php echo "img/".$val['imgs']; ?> class="card-img-top " alt="something rong" style="width: 15rem;">
+      <div class="card h-100 card text-dark bg-light mb-3">
+          <img src=<?php echo "img/".$val['imgs']; ?> class="card-img-top " alt="something rong">
           <div class="card-body">
             <h5 class="card-title"><?php echo $val['title']; ?></h5>
             <p class="card-text"><?php echo $val['detail']; ?></p>
-              <button class="btn btn-primary">source code</button>
+            <a href='https://github.com/Mirko-khatab/webproject2'><button class="btn btn-primary">Showing</button></a>
+            <a href='https://github.com/Mirko-khatab/webproject2'><button class="btn btn-secondary">source code</button></a>
           </div>
          </div>
         </div>
@@ -51,6 +52,3 @@
     </div>
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
